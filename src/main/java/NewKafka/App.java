2eb453c908c65177805d6505833c01d48f4620c3
disawalsagar <disawalsagar@yahoo.com>
2016-06-01 04:03:05 -0400

    initial

---
+++ src/main/java/NewKafka/App.java
@@ -0,0 +1,16 @@
+package NewKafka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        switch ("Hello"){
+
+        }
+    }
+}
